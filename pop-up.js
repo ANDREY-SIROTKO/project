7c72eb2925f613e8b5250a3cccfcebbcfafae60f
ANDREY-SIROTKO <91492771+ANDREY-SIROTKO@users.noreply.github.com>
2022-01-18 03:16:44 +0300

    файлы для сайта

---
+++ pop-up.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", docReady);
+function docReady()
+{
+    sessionStorage.setItem('is_reloaded', 'no');
+    if(sessionStorage.getItem('is_reloaded') == 'no')
+    {
+        sessionStorage.setItem('is_reloaded', 'yes');
+        setTimeout(showPop, 4000);
+    }
+}
+function showPop()
+{
+    document.getElementById("myForm").style.display = "block";
+    sessionStorage.setItem('is_reloaded', 'no');
+}
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+ }
+
+
+
+
+
+
+
+
+
+
+       
